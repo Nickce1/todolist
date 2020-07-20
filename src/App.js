f44e0bc5d95cbf01cf159e85151b67a747fcf1f0
Nickce <f.lineup@gmail.com>
2020-07-20 11:54:53 +0400

    Query and display all tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { Navigation } from './Components/Navigation'
+import { TasksList } from './Components/TasksList'
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  const getAlltasks = () => {
+    fetch('http://localhost:4000')
+      .then((response) => response.json())
+      .then((response) => {
+        setTasks(response.data)
+      })
+  }
+
+  useEffect(() => {
+    getAlltasks()
+  }, [])
+
   return (
     <div className="App">
       <Navigation />
+      <TasksList tasks={tasks} />
     </div>
   )
 }
